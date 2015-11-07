0a957db3c729baec5b8f2cc12680e9e24cf35a6a
Grigory Ozhegov <grigory@ozhegov.name>
2015-11-07 04:22:11 +0300

    new to_dict implementation: allow remove fields also from embedded models, compatible with mongo fields selection

---
+++ tornkts/mixins/to_dict_mixin.py
@@ -0,0 +1,53 @@
+from tornkts.base.server_response import ServerError
+
+
+class ToDictMixin(object):
+    MODE_INCLUDE = 1
+    MODE_EXCLUDE = 0
+
+    def to_dict(self, name='', *args, **kwargs):
+        depth = kwargs.get('depth', 0)
+        kwargs.update({'depth': depth + 1})
+
+        if depth == 0:
+            fields = kwargs.get('fields', None)
+        else:
+            fields = kwargs.get(name + '_fields', None)
+
+        next_fields = {}
+        if isinstance(fields, dict):
+            for k, mode in fields.items():
+                field = k.split('.', 1)
+                if len(field) > 1:
+                    entity_title = field[0]
+                    fields_chain = field[1]
+                    kwargs_key = entity_title + '_fields'
+
+                    if mode == ToDictMixin.MODE_INCLUDE:
+                        fields[field[0]] = mode
+                    if next_fields.get(kwargs_key) is None:
+                        next_fields[kwargs_key] = {}
+                    next_fields[kwargs_key][fields_chain] = mode
+
+        kwargs.update(next_fields)
+        dict_object = self.to_dict_impl(**kwargs)
+
+        if isinstance(fields, dict):
+            mode = None
+            for v in fields.values():
+                if v != mode and mode is not None:
+                    raise ServerError('mix_fields_filter')
+                mode = v % 2  # MODE_EXCLUDE or MODE_INCLUDE
+
+            for field in dict_object.keys():
+                if mode == ToDictMixin.MODE_INCLUDE:
+                    if field not in fields.keys():
+                        dict_object.pop(field, False)
+                elif mode == ToDictMixin.MODE_EXCLUDE:
+                    if field in fields.keys():
+                        dict_object.pop(field, False)
+
+        return dict_object
+
+    def to_dict_impl(self, **kwargs):
+        return {}
