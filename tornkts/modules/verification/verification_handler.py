15ce18094d4cf25b354a959367be6a1686ccfa5f
Sergey Chernobrovkin <sergey.chernobrovkin@inbox.ru>
2015-12-09 15:08:25 +0300

    [+] verification module

---
+++ tornkts/modules/verification/verification_handler.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+from tornkts.base.mongodb import get_object_or_none
+from tornkts.base.server_response import ServerError, ServerResponseStatus
+from tornkts.handlers import BaseHandler
+from tornkts.modules.verification import Verification
+
+
+class VerificationHandler(BaseHandler):
+
+    STATUSES = [
+        ServerResponseStatus('sms_send_fail', 'fail to send some sms', 406),
+        ServerResponseStatus('verification_not_found', 'no requested verification', 400),
+        ServerResponseStatus('invalid_verification_key', 'incorrect verification key', 403),
+    ]
+
+    def keygen(self):
+        import random
+        return unicode(random.randint(10000, 100000))
+
+    def send(self, verification):
+        """
+        Метод отправки пользователю ключа
+        """
+        raise NotImplementedError
+
+    def get_verified_argument(self):
+        """
+        Получение телефона/email'а и тд
+        :return:
+        """
+        return self.get_str_argument("phone")
+
+    @property
+    def post_methods(self):
+        return {
+            'verify': self.__verify
+        }
+
+    @property
+    def get_methods(self):
+        return {
+            'get_key': self.get_verification_key,
+        }
+
+    def get_verification_key(self):
+        verified_entity = self.get_verified_argument()
+
+        verification = get_object_or_none(Verification, verified_entity=verified_entity)
+        if verification is None:
+            verification = Verification.generate(verified_entity, self.keygen)
+            verification.save()
+
+        self.send(verification)
+
+        self.send_success_response()
+
+    @staticmethod
+    def verify(verified_entity, verification_key):
+        """
+        Метод должен райзить ошибки
+        :param verified_entity: сущность
+        :param verification_key: ключ
+        :return:
+        """
+        verification = get_object_or_none(Verification, verified_entity=verified_entity)
+
+        if verification is None:
+            raise ServerError('verification_not_found')
+        if not verification.verify(verification_key):
+            raise ServerError('invalid_verification_key')
+
+        verification.delete()
+
+    def __verify(self):
+        verified_entity = self.get_verified_argument()
+        verification_key = self.get_str_argument("verification_key")
+
+        self.verify(verified_entity, verification_key)
+
+        self.send_success_response()
